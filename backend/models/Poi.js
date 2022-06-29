a5dd60d498924438707358396ab06f74be3a319c
Justin Lam <justin.kh.lam@gmail.com>
2022-06-29 16:48:55 -0400

    add in POI schema

--- backend/models/Poi.js
+++ backend/models/Poi.js
@@ -1,1 +1,38 @@
 const mongoose = require("mongoose");
+
+const PoiSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+    },
+    title: {
+      type: String,
+      require: true,
+      min: 3,
+    },
+    description: {
+      type: String,
+      require: true,
+      min: 3,
+    },
+    rating: {
+      type: Number,
+      require: true,
+      min: 0,
+      max: 5,
+    },
+    lat: {
+      type: Number,
+      require: true,
+    },
+    long: {
+      type: Number,
+      require: true,
+    },
+  },
+  // adds creation/update info
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("POI", PoiSchema);
