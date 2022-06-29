b093844fe0628c4930f4018d4863355076401806
Justin Lam <justin.kh.lam@gmail.com>
2022-06-29 16:40:07 -0400

    update .gitignore. add in mongoose connect to .env file

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URL, { useNewUrlParser: true })
+  .then(() => {
+    console.log("MongoDB connected! 🐯");
+  })
+  .catch((err) => console.log(`MongoDB Error`, err));
 
 app.listen(8080, () => {
   console.log(`Express server is running on Port 8080! 🐅`);
