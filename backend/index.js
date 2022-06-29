2b186c38c58bcfca6daf04a3aa7a964d450c5da0
Justin Lam <justin.kh.lam@gmail.com>
2022-06-28 23:29:21 -0400

    initialize express server on index.js

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.listen(8080, () => {
+  console.log(`Express server is running on Port 8080!`);
+});
