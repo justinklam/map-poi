b108eee34925f985e2d7b0c13170d48d0db9706a
Justin Lam <justin.kh.lam@gmail.com>
2022-07-01 01:12:49 -0400

    add logic to create new user with email/hashed password

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -11,7 +11,14 @@ router.post("/register", async (req, res) => {
     // generate new password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
     // create a new user
+    const newUser = new User({
+      username: res.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
     // save the user to db and send response
   } catch (err) {
     res.status(500).json(err);
