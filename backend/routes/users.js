904fcb3330a094f0789dd56d38b70d3ac155ddfa
Justin Lam <justin.kh.lam@gmail.com>
2022-07-02 00:30:19 -0400

    add in incorrect password return. add in successful return for correct password

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -35,14 +35,16 @@ router.post("/login", async (req, res) => {
     const user = await User.findOne({ username: req.body.username });
 
     // if user is not found
-    !user && res.status(400).json("Incorrect login credentials");
+    !user && res.status(400).json("Incorrect login credentials!");
     // validate password
     const validatePassword = await bcrypt.compare(
       req.body.password,
       user.password
     );
 
-    !validatePassword && res.status(400).json("Incorrect login credentials");
+    !validatePassword && res.status(400).json("Incorrect login credentials!");
+
+    res.status(200).json({ _id: user._id, username: username });
 
     // if validation is sucessful, send res
   } catch (err) {
