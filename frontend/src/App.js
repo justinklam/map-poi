69ba68b6a3131780ec3003db6a410201e04a1db3
Justin Lam <justin.kh.lam@gmail.com>
2022-07-03 11:01:26 -0400

    add in mapbox to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,23 @@
+import { useState } from "react";
+import ReactMapGL from "react-map-gl";
+
 function App() {
-  return <div className="App">Hello world</div>;
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8,
+  });
+
+  return (
+    <div className="App">
+      <ReactMapGL
+        {...viewport}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      />
+    </div>
+  );
 }
 
 export default App;
