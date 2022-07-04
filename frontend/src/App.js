d37804748e61533b38360937fcbd4bc5500b41c8
Justin Lam <justin.kh.lam@gmail.com>
2022-07-04 00:18:06 -0400

    add in marker component as test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
     height: "100vh",
     latitude: 43.65107,
     longitude: -79.347015,
-    zoom: 8,
+    zoom: 12,
   });
 
   return (
@@ -16,7 +16,16 @@ function App() {
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
         onViewportChange={(nextViewport) => setViewport(nextViewport)}
-      ></ReactMapGL>
+      >
+        <Marker
+          latitude={43.6548}
+          longitude={-79.3726}
+          offsetLeft={-20}
+          offsetTop={-10}
+        >
+          <div>You are here</div>
+        </Marker>
+      </ReactMapGL>
     </div>
   );
 }
