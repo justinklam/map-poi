4c6960fd0f948e2a52103c755182ea80c66e58e7
Justin Lam <justin.kh.lam@gmail.com>
2022-07-13 13:40:22 -0400

    add in logo from MUI. add in color for logo

--- frontend/src/components/register/Register.jsx
+++ frontend/src/components/register/Register.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./register.css";
 
+// MUI
+import { Room } from "@material-ui/icons";
+
 const Register = () => {
   return (
     <div className="registerContainer">
-      <div className="logo"></div>
+      <div className="logo">
+        <Room />
+        Map-POI
+      </div>
       <form>
         <input type="text" placeholder="username" />
         <input type="email" placeholder="email" />
