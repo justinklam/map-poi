9a5dd14f4a6a9a1ef4738e55260646a61ae3e862
Justin Lam <justin.kh.lam@gmail.com>
2022-07-14 00:36:27 -0400

    add classname to register btn. add styling

--- frontend/src/components/register/Register.jsx
+++ frontend/src/components/register/Register.jsx
@@ -15,7 +15,7 @@ const Register = () => {
         <input type="text" placeholder="username" />
         <input type="email" placeholder="email" />
         <input type="password" placeholder="password" />
-        <button>Register</button>
+        <button className="registerBtn">Register</button>
       </form>
     </div>
   );
